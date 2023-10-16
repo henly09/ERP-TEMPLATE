83b68f68cfe77534928806831df73708772906b0
henly09 <montera@gmail.com>
2023-10-16 22:07:41 +0800

    Fixed the inputmask of amount in vouchreq

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,9 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import {initTE, Carousel, LoadingManagement, Sidenav, Select, Ripple} from "tw-elements";
 import swal from 'sweetalert2';
-import jQuery from 'jquery';
+
 
 window.Swal = swal;
-window.$ = jQuery;
 window.Alpine = Alpine;
 Alpine.start();
 initTE({ Carousel, LoadingManagement, Sidenav, Select, Ripple });
